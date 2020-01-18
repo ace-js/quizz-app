38704e19e7ca9bc9998b43c4e935269ca6c56bdb
Arnaud Bertieaux <arnaud@Arnauds-MacBook-Air.local>
2020-01-18 21:41:13 +0100

    Replaced basic constructor by constructor with Dart syntax sugar

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,10 @@ class Question {
   String description;
   bool answer;
 
-  Question(String description, bool answer) {
-    this.description = description;
-    this.answer = answer;
-  }
+  Question({this.description, this.answer}); // Syntax sugar that does the same as the commented constructor
+
+//  Question({String description, bool answer}) {
+//    this.description = description;
+//    this.answer = answer;
+//  }
 }
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -5,31 +5,48 @@ class QuizBrain {
   int _score = 0;
 
   List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
     Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
-        false),
+        description: 'Some cats are actually allergic to humans', answer: true),
     Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
+        description: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
     Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
+        description:
+            'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(description: 'A slug\'s blood is green.', answer: true),
     Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
-    Question('Google was originally called \"Backrub\".', true),
+        description: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+        answer: true),
     Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
+        description: 'It is illegal to pee in the Ocean in Portugal.',
+        answer: true),
     Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true)
+        description:
+            'No piece of square dry paper can be folded in half more than 7 times.',
+        answer: false),
+    Question(
+        description:
+            'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        answer: true),
+    Question(
+        description:
+            'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        answer: false),
+    Question(
+        description:
+            'The total surface area of two human lungs is approximately 70 square metres.',
+        answer: true),
+    Question(
+        description: 'Google was originally called \"Backrub\".', answer: true),
+    Question(
+        description:
+            'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        answer: true),
+    Question(
+        description:
+            'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        answer: true)
   ];
 
   bool hasMoreQuestion() {
